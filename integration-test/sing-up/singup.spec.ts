0b35ec9d10b1499a5ba5bcc34f26a05905f7f42e
abhutraKB <ashish.bhutra@khatabook.com>
2022-03-15 15:11:17 +0530

    Fixed test

--- integration-test/sing-up/singup.spec.ts
+++ integration-test/sing-up/singup.spec.ts
@@ -1,4 +1,5 @@
-const req = require('../baseTest')
+const supertest = require('supertest')
+const request = supertest('https://api.bizanalyst.in/user')
 const testObject = require('../../test-data/user.json')
 
 import { SignUpData } from '../../models/sign-up/request/signup-data'
@@ -6,13 +7,10 @@ import { SignUpData } from '../../models/sign-up/request/signup-data'
 describe('Signup', () => {
   it.each(testObject)('Sign up test', (signupData: any) => {
     const inputData: SignUpData = signupData.input
-    req.request
+    request
       .post('/')
       .set('Content-Type', 'application/json')
       .send(inputData)
       .expect(signupData.response)
-      .then((res: any) => {
-        console.log(JSON.stringify(res.status))
-      })
   })
 })
